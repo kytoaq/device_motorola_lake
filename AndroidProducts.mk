039c5dcf7fba8c9f0c8223da4a4f964e0fbb15b5
kytoaq <kytoaq@gmail.com>
2020-12-18 07:42:43 +0000

    lake: AOSiP-ify
    Signed-off-by: kytoaq <kytoaq@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lake.mk
+    $(LOCAL_DIR)/aosip_lake.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lake-eng \
-    lineage_lake-user \
-    lineage_lake-userdebug
+    aosip_lake-eng \
+    aosip_lake-user \
+    aosip_lake-userdebug
